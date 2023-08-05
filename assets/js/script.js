42bc6e663b4f309d771fe7b4f5837317811c4f66
harry-c26 <>
2023-08-05 11:20:12 +0000

    Updated titled

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,3 +136,4 @@ function displayDivisionQuestion(operand1, operand2) {
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
 }
+
